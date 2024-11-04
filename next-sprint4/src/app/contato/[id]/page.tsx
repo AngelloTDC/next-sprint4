03db366f35ed721f0c71800a01e261eaa5189a2e
Angello Turano <161774225+AngelloTDC@users.noreply.github.com>
2024-11-04 19:09:27 -0300

    teste

--- next-sprint4/src/app/contato/[id]/page.tsx
+++ next-sprint4/src/app/contato/[id]/page.tsx
@@ -6,16 +6,15 @@ export default async function ContatoUnidade({
 }: {
   params: { id: string };
 }) {
-  const { id } = await params;
+  const id = params.id;
 
-  const unidadeDescricao = getContactById(id);
+  const unidadeDescricao = await getContactById(id);
 
   return (
     <div className="max-w-2xl mx-auto p-8 bg-gradient-to-b from-blue-500 to-teal-500 rounded-3xl shadow-lg">
       <h1 className="text-3xl font-bold text-center text-white mb-6">
         Fale com a {unidadeDescricao?.nome}
       </h1>
-
       <FormContact />
     </div>
   );
