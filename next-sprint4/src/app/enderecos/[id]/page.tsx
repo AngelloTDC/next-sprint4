5d509b4589d0f39a4e3f2a56acd3013fe4e2f0c4
Angello Turano <73845257+IHateMyTiming@users.noreply.github.com>
2024-11-04 18:48:50 -0300

    tamanho do mapa

--- next-sprint4/src/app/enderecos/[id]/page.tsx
+++ next-sprint4/src/app/enderecos/[id]/page.tsx
@@ -21,7 +21,8 @@ export default async function EnderecoDetalhe({
         </h1>
         <Link
           href="/enderecos"
-          className="mt-6 px-4 py-2 bg-blue-700 text-white rounded-lg hover:bg-blue-800 transition duration-200">
+          className="mt-6 px-4 py-2 bg-blue-700 text-white rounded-lg hover:bg-blue-800 transition duration-200"
+        >
           Voltar para a lista de endereços
         </Link>
       </div>
@@ -29,27 +30,28 @@ export default async function EnderecoDetalhe({
   }
 
   return (
-    <div className="flex flex-col items-center text-center mt-10">
-      <div className="p-8 bg-white rounded-lg shadow-lg max-w-lg">
+    <div className="flex flex-col items-center text-center mt-10 w-full">
+      <div className="p-8 bg-white rounded-lg shadow-lg max-w-[532px] w-full">
         <h1 className="text-3xl font-bold mb-4 text-blue-500">
           {endereco.title}
         </h1>
         <p className="text-lg text-gray-700 mb-6">{endereco.description}</p>
 
-        <div className="w-full h-64 rounded-lg shadow-md relative bg-gray-200">
+        <div className="w-full h-64 rounded-lg shadow-md relative bg-gray-200 min-w-80 max-w-[500px]">
           <iframe
             src={`https://www.google.com/maps?q=${endereco.latitude},${endereco.longitude}&z=15&output=embed`}
             width="100%"
             height="100%"
             loading="lazy"
-            className="rounded-lg">
-          </iframe>
+            className="min-w-80 max-w-[500px] w-full"
+          ></iframe>
         </div>
       </div>
 
       <Link
         href="/enderecos"
-        className="mt-6 px-4 py-2 bg-blue-700 text-white rounded-lg hover:bg-blue-800 transition duration-200">
+        className="mt-6 px-4 py-2 bg-blue-700 text-white rounded-lg hover:bg-blue-800 transition duration-200"
+      >
         Voltar para a lista de endereços
       </Link>
     </div>
